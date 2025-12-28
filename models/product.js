4561d64ae249e363b1284d21ff4450de174fb4d5
sazidahmed0912-ui <sazidahmed0912@gmail.com>
2025-12-28 21:39:28 +0530

    Add gitignore and clean repository

--- models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Product", new mongoose.Schema({
-  name:String,
-  price:Number,
-  image:String,
-  stock:Number,
-  description:String
-}));
--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("User", new mongoose.Schema({
-  name:String,
-  email:String,
-  password:String,
-  isAdmin:{ type:Boolean, default:false }
-}));
